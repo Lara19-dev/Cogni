0aeb71a39493a66d050510f6a6b632d7df6856cc
Senre <44935191+Senre@users.noreply.github.com>
2019-08-26 10:19:36 +0800

    Update Custom Status

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = client => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('to Impress Impy', { type: 'PLAYING' })
+    client.user.setActivity('excurb1a crusading wisdom', { type: 'WATCHING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
     .catch(console.error);
 };
