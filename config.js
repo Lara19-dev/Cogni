8a6224b4cfeb2517dfbfb22c9f0c9fe73229528e
Senre <44935191+Senre@users.noreply.github.com>
2019-08-26 10:08:09 +0800

    App's backbone etc

---
+++ config.js
@@ -0,0 +1,17 @@
+require('dotenv-flow').config();
+
+module.exports = {
+
+    token: process.env.TOKEN,
+    owner: process.env.OWNER,
+    prefix: process.env.PREFIX,
+    botVersion: process.env.BOTVERSION,
+
+    // "servers": [
+    //     "596905593365725239", "554808784531488789"],
+    // "roleName": "Rainbow",
+    // "colors": 36,
+    // "speed": 1000,
+    // "logging": false
+    "fRoleName": "Newcomers"
+};
